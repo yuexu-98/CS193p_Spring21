d6105112566b437f19b45774f19cc1c4188ee694
yuexu <jessicaxu0516@gmail.com>
2022-03-04 19:28:35 +0800

    Initial commit

---
+++ Memorize/Memorize/ContentView.swift
@@ -0,0 +1,116 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Jane Adrea on 2022/3/3.
+//
+
+import SwiftUI
+
+/*
+    "ContentView": name of the data structure
+    ": View": the struct we created behaves like a View
+ */
+struct ContentView: View {
+    /*
+     ": some View": the type of the variable
+        If we return a Text here, we can use
+        var body: Text
+     Why use some View here? We may return complicated object here
+     
+     declare a variable of type Int -> "var a: Int"
+     */
+    
+    var emojis: Array<String> = ["☺️", "🙃", "😚"]
+    @State var emojiCount: Int = 2
+    
+    var body: some View {
+        VStack{
+            HStack{
+                ForEach(emojis[0..<emojiCount], id: \.self){ emoji in
+                    CardView(content: emoji)
+                }
+            }
+            Spacer()
+            HStack{
+                remove
+                Spacer()
+                add
+            }
+            .padding(.horizontal)
+            .font(.largeTitle)
+        }
+        .padding(.horizontal)
+        .foregroundColor(.red)
+    }
+    
+    var remove: some View {
+        Button(action: {
+            if emojiCount > 1{
+                emojiCount -= 1
+            }
+            
+        }, label: {
+            Image(systemName: "minus.circle")
+        })
+    }
+    
+    var add: some View {
+        Button(action: {
+            if emojiCount < emojis.count{
+                emojiCount += 1
+            }
+        }, label: {
+            Image(systemName: "plus.circle")
+        })
+    }
+}
+
+/*
+    All view is immutable in SwiftUI!!
+    that's why we can't change isFaceUp inside the body
+    -> use @State to isFaceUp
+ */
+struct CardView: View{
+    /*
+        hiden return statement
+        => return Text("Hello, world!")
+        But here we use padding -> the return type is not Text!!
+        =>Thus we use "some View" as return type here
+     
+        we can ignore the "return" here, since we only have one component here
+        we can use {} instead of using content:{} as argument
+     */
+
+    var content: String
+    @State var isFaceUp: Bool = true
+    
+    var body: some View{
+        ZStack(alignment: .center)  {
+            // we can omit the type here
+            let shape = RoundedRectangle(cornerRadius: 20.0)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.stroke(lineWidth: 3.0)
+                Text(content).font(.largeTitle)
+            }else{
+                shape.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
+    
+
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
+    }
+}
---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Jane Adrea on 2022/3/3.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
